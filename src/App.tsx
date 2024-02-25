00f333a2f6f5d6f566f1065a90d6fedaae4dfa1c
JakobLarsNaurHansen <s224312@student.dtu.dk>
2024-02-25 13:29:38 +0100

    Small changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,97 @@
-import CustomerItem from './components/CustomerItem';
-import BasketSummary from './components/BasketSummary';
-import { RecurringOrder } from './types/Types'
-import initialBasketItems from './data';
+import CustomerItem from "./components/CustomerItem";
+import BasketSummary from "./components/BasketSummary";
+import { RecurringOrder } from "./types/Types";
+import initialBasketItems from "./data";
 import { useState } from "react";
-import './index.css';
+import "./index.css";
 
-const headerNames = ["Type", "Price", "#", "Sum", "Options"]
+const headerNames = ["Type", " ,- /stk.", "Quantity", "Sum", "Options"];
 function App() {
-	const [basketItems, setBasketItems] = useState(initialBasketItems);
+  const [basketItems, setBasketItems] = useState(initialBasketItems);
 
-	for (let item of basketItems.slice(0, 3)) {
-		console.log(item);
-	}
-	const handleQuantityChange = (itemId: number, newQuantity: number) => {
-		if (newQuantity < 1) { return }
-		const updatedItems = basketItems.map(item => {
-			if (item.id === itemId) {
-				return { ...item, quantity: newQuantity };
-			}
-			return item;
-		});
-		setBasketItems(updatedItems);
-	};
+  for (let item of basketItems.slice(0, 3)) {
+    console.log(item);
+  }
+  const handleQuantityChange = (itemId: number, newQuantity: number) => {
+    if (newQuantity < 1) {
+      return;
+    }
+    const updatedItems = basketItems.map((item) => {
+      if (item.id === itemId) {
+        return { ...item, quantity: newQuantity };
+      }
+      return item;
+    });
+    setBasketItems(updatedItems);
+  };
 
-	const handleGiftWrapChange = (itemId: number) => {
-		const updatedItems = basketItems.map(item => {
-			if (item.id === itemId) {
-				return { ...item, giftWrap: !item.giftWrap };
-			}
-			return item;
-		});
-		setBasketItems(updatedItems);
-	};
+  const handleGiftWrapChange = (itemId: number) => {
+    const updatedItems = basketItems.map((item) => {
+      if (item.id === itemId) {
+        return { ...item, giftWrap: !item.giftWrap };
+      }
+      return item;
+    });
+    setBasketItems(updatedItems);
+  };
 
-	const handleRecurringOrderChange = (itemId: number, newRecurringOrder: RecurringOrder) => {
-		if (Object.values(RecurringOrder).includes(newRecurringOrder)) {
-			const updatedBasketItems = basketItems.map(item =>
-				item.id === itemId ? { ...item, recurringOrder: newRecurringOrder as RecurringOrder } : item
-			);
-			setBasketItems(updatedBasketItems);
-		} else {
-			console.error("Invalid recurring order type.");
-		}
-	}
+  const handleRecurringOrderChange = (
+    itemId: number,
+    newRecurringOrder: RecurringOrder
+  ) => {
+    if (Object.values(RecurringOrder).includes(newRecurringOrder)) {
+      const updatedBasketItems = basketItems.map((item) =>
+        item.id === itemId
+          ? { ...item, recurringOrder: newRecurringOrder as RecurringOrder }
+          : item
+      );
+      setBasketItems(updatedBasketItems);
+    } else {
+      console.error("Invalid recurring order type.");
+    }
+  };
 
-	const handleRemove = (itemId: number) => {
-		setBasketItems(basketItems.filter(item => item.id !== itemId));
-	}
+  const handleRemove = (itemId: number) => {
+    setBasketItems(basketItems.filter((item) => item.id !== itemId));
+  };
 
-	return (
-		<div className="basket-container">
-			<h1>Shopping Basket</h1>
-			<table>
-				<caption>
-					Items you have picked
-				</caption>
-				<Header headerNames={headerNames} />
-				<tbody>
-					{basketItems.map((item) => (
-						<CustomerItem
-							key={item.id}
-							item={item}
-							onQuantityChange={handleQuantityChange}
-							onGiftWrapChange={handleGiftWrapChange}
-							onRecurringOrderChange={handleRecurringOrderChange}
-							onRemove={() => handleRemove(item.id)}
-						/>
-					))}
-				</tbody>
-
-			</table>
-			<BasketSummary items={basketItems} />
-		</div>
-	);
+  return (
+    <div className="basket-container">
+      <h1>Shopping Basket</h1>
+      <table>
+        <caption></caption>
+        <Header headerNames={headerNames} />
+        <tbody>
+          {basketItems.map((item) => (
+            <CustomerItem
+              key={item.id}
+              item={item}
+              onQuantityChange={handleQuantityChange}
+              onGiftWrapChange={handleGiftWrapChange}
+              onRecurringOrderChange={handleRecurringOrderChange}
+              onRemove={() => handleRemove(item.id)}
+            />
+          ))}
+        </tbody>
+      </table>
+      <BasketSummary items={basketItems} />
+    </div>
+  );
 }
 
 interface HeaderProps {
-	headerNames: string[];
+  headerNames: string[];
 }
 function Header({ headerNames }: HeaderProps) {
-	return (
-		<thead>
-			<tr>
-				{headerNames.map((headerName: string) => <th key={headerName}>{headerName}</th>)}
-			</tr>
-		</thead>
-	)
-
+  return (
+    <thead>
+      <tr>
+        {headerNames.map((headerName: string) => (
+          <th key={headerName}>{headerName}</th>
+        ))}
+      </tr>
+    </thead>
+  );
 }
 
 export default App;
--- src/components/CustomerItem.tsx
+++ src/components/CustomerItem.tsx
@@ -1,44 +1,60 @@
-import { BasketItemProps, RecurringOrder } from "../types/Types"
+import { BasketItemProps, RecurringOrder } from "../types/Types";
 import { GiftSvg } from "../assets/GiftSvg";
+import "../index.css";
 
+export default function CustomerItem({
+  item,
+  onGiftWrapChange,
+  onRecurringOrderChange,
+  onQuantityChange,
+  onRemove,
+}: BasketItemProps) {
+  return (
+    <tr key={item.id}>
+      <td align="right">{item.name}</td>
+      <td>{item.price},-</td>
+      <td align="center">
+        <button
+          className="quantity-btn"
+          onClick={() => onQuantityChange(item.id, item.quantity - 1)}
+        >
+          -
+        </button>
+        <span className="quantity-text">{item.quantity}</span>
+        <button
+          className="quantity-btn"
+          onClick={() => onQuantityChange(item.id, item.quantity + 1)}
+        >
+          +
+        </button>
+      </td>
+      <td>{item.price * item.quantity},-</td>
+      <td className="options-cell">
+        <label htmlFor={"gift-" + item.id}>
+          <GiftSvg isToggled={item.giftWrap} />
+          <input
+            id={"gift-" + item.id}
+            type="checkbox"
+            onClick={() => onGiftWrapChange(item.id)}
+          />
+        </label>
 
-export default function CustomerItem({ item, onGiftWrapChange, onRecurringOrderChange, onQuantityChange, onRemove }: BasketItemProps) {
-
-
-	return (
-		<tr key={item.id}>
-			<td align="right">
-				{item.name}
-			</td>
-			<td>{item.price},-</td>
-			<td>
-				<button className="dec-button"
-					onClick={() => onQuantityChange(item.id, item.quantity - 1)}>-</button>
-				{item.quantity}
-				<button className="dec-button" onClick={() => onQuantityChange(item.id, item.quantity + 1)}>+</button>
-			</td>
-			<td>{item.price * item.quantity},-</td>
-			<td className="options-cell">
-
-				<label htmlFor={"gift-" + item.id}>
-					<GiftSvg isToggled={item.giftWrap} />
-					<input
-						id={"gift-" + item.id}
-						type="checkbox"
-						onClick={() => onGiftWrapChange(item.id)}
-					/>
-
-				</label>
-
-				<select value={item.recurringOrder} onChange={(e) => onRecurringOrderChange(item.id, parseInt(e.target.value) as RecurringOrder)}>
-					<option value={RecurringOrder.Once}>Once</option>
-					<option value={RecurringOrder.Daily}>Daily</option>
-					<option value={RecurringOrder.Weekly}>Weekly</option>
-					<option value={RecurringOrder.Biweekly}>Biweekly</option>
-				</select>
-				<button onClick={onRemove}>X</button>
-			</td>
-
-		</tr >
-	);
+        <select
+          value={item.recurringOrder}
+          onChange={(e) =>
+            onRecurringOrderChange(
+              item.id,
+              parseInt(e.target.value) as RecurringOrder
+            )
+          }
+        >
+          <option value={RecurringOrder.Once}>Once</option>
+          <option value={RecurringOrder.Daily}>Daily</option>
+          <option value={RecurringOrder.Weekly}>Weekly</option>
+          <option value={RecurringOrder.Biweekly}>Biweekly</option>
+        </select>
+        <button onClick={onRemove}>x</button>
+      </td>
+    </tr>
+  );
 }
