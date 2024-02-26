67779313491d39b52683301972500ec1b509218b
JakobLarsNaurHansen <s224312@student.dtu.dk>
2024-02-26 16:12:20 +0100

    first test

--- src/test/app.test.tsx
+++ src/test/app.test.tsx
@@ -1,14 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
-import React from 'react';
+import React from "react";
 import App from "../App";
-import '@testing-library/jest-dom';
-
 
 describe(App.name, () => {
-    test("should display", () => {
-        render(<App />);
-        const element = screen.getByText("Shopping Basket");
-        expect(element).toBeInTheDocument();
-    });
+  test("should display", () => {
+    render(<App />);
+    const element = screen.getByText("Shopping Basket");
+    expect(element).toBeInTheDocument();
+  });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import '@testing-library/jest-dom';
 
 export default defineConfig({
 	plugins: [react()],
