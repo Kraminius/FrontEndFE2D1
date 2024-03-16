5577248e4aca8d30075dbadd1dce47f3349c326b
JakobLarsNaurHansen <s224312@student.dtu.dk>
2024-03-16 15:52:24 +0100

    more tests

--- src/test/delivery.test.tsx
+++ src/test/delivery.test.tsx
@@ -58,4 +58,73 @@ describe(Delivery.name, () => {
 
     expect(getByText("Submit")).toBeEnabled();
   });
+
+  test("disnables submit button when form is valid, but different billing address is chosen", async () => {
+    const { getByLabelText, getByText } = render(<Delivery />);
+
+    fireEvent.change(getByLabelText(/First Name \*/i), {
+      target: { value: "Jakob" },
+    });
+    fireEvent.change(getByLabelText(/Last Name \*/i), {
+      target: { value: "Hansen" },
+    });
+    fireEvent.change(getByLabelText(/Email \*/i), {
+      target: { value: "Jakob@example.com" },
+    });
+    fireEvent.change(getByLabelText(/Phone \*/i), {
+      target: { value: "12345678" },
+    });
+    fireEvent.change(getByLabelText(/Address \*/i), {
+      target: { value: "Test Street 69" },
+    });
+    fireEvent.change(getByLabelText(/City \*/i), {
+      target: { value: "København Ø" },
+    });
+    fireEvent.change(getByLabelText(/Zip Code \*/i), {
+      target: { value: "2100" },
+    });
+    const billingAddressCheckbox = getByLabelText(/Different billing address/);
+    fireEvent.click(billingAddressCheckbox);
+
+    expect(getByText("Submit")).toBeDisabled();
+  });
+
+  test("Enables submit button when form is valid, different billing address is chosen and form filled", async () => {
+    const { getByLabelText, getByText } = render(<Delivery />);
+
+    fireEvent.change(getByLabelText(/First Name \*/i), {
+      target: { value: "Jakob" },
+    });
+    fireEvent.change(getByLabelText(/Last Name \*/i), {
+      target: { value: "Hansen" },
+    });
+    fireEvent.change(getByLabelText(/Email \*/i), {
+      target: { value: "Jakob@example.com" },
+    });
+    fireEvent.change(getByLabelText(/Phone \*/i), {
+      target: { value: "12345678" },
+    });
+    fireEvent.change(getByLabelText(/Address \*/i), {
+      target: { value: "Test Street 69" },
+    });
+    fireEvent.change(getByLabelText(/City \*/i), {
+      target: { value: "København Ø" },
+    });
+    fireEvent.change(getByLabelText(/Zip Code \*/i), {
+      target: { value: "2100" },
+    });
+    const billingAddressCheckbox = getByLabelText(/Different billing address/);
+    fireEvent.click(billingAddressCheckbox);
+    fireEvent.change(getByLabelText(/Billing Address \*/i), {
+      target: { value: "Test Street 420" },
+    });
+    fireEvent.change(getByLabelText(/Billing City \*/i), {
+      target: { value: "Askeby" },
+    });
+    fireEvent.change(getByLabelText(/Billing Zip Code \*/i), {
+      target: { value: "4792" },
+    });
+
+    expect(getByText("Submit")).toBeEnabled();
+  });
 });
