4eda1f573b88285650222a9ebc3301d08616aa98
Henrik <henrik.zenkert@gmail.com>
2024-04-16 09:57:25 +0200

    ran linter

--- src/components/summary/OrderSummary.tsx
+++ src/components/summary/OrderSummary.tsx
@@ -3,20 +3,19 @@ import BasketSummary from "./BasketSummary.tsx";
 import SummaryItem from "./SummaryItem.tsx";
 
 function OrderSummary({ items }: BasketSummaryProps) {
-	return (
-		<div id='summary-wrapper'>
-			<div id="summary-container">
-				<h2>Order Summary</h2>
-				<ul className="item-list">
-					{items.map((item, index) => (
-						<SummaryItem key={index} item={item} />
-					))}
-				</ul>
-				<BasketSummary items={items} />
-
-			</div>
-		</div>
-	);
+  return (
+    <div id="summary-wrapper">
+      <div id="summary-container">
+        <h2>Order Summary</h2>
+        <ul className="item-list">
+          {items.map((item, index) => (
+            <SummaryItem key={index} item={item} />
+          ))}
+        </ul>
+        <BasketSummary items={items} />
+      </div>
+    </div>
+  );
 }
 
 export default OrderSummary;
