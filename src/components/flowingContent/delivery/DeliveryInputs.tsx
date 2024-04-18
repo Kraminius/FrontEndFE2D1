1643f534d8f08b26dfa636ac72059b1e5d7ca373
JakobLarsNaurHansen <s224312@student.dtu.dk>
2024-04-18 12:59:11 +0200

    Added provider in main

--- src/components/flowingContent/delivery/DeliveryInputs.tsx
+++ src/components/flowingContent/delivery/DeliveryInputs.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import countries from "./countries";
 import { useDeliveryDispatchContext, useDeliveryContext } from "../../../context/DeliveryContext";
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import {BasketProvider} from "./context/BasketContext.tsx";
+import { DeliveryProvider } from "./context/DeliveryContext.tsx";
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <BasketProvider>
-            <App />
+            <DeliveryProvider>
+                <App />
+            </DeliveryProvider>
         </BasketProvider>
     </React.StrictMode>,
 );
