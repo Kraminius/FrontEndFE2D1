a00c215675838358d8c7278184a3840c9034731a
notRolle <rolle474@gmail.com>
2024-04-18 12:04:13 +0200

    Not working yet

--- src/components/summary/OrderSummary.tsx
+++ src/components/summary/OrderSummary.tsx
@@ -1,21 +1,23 @@
-import { BasketSummaryProps } from "../../types/Types.ts";
 import BasketSummary from "./BasketSummary.tsx";
 import SummaryItem from "./SummaryItem.tsx";
+import {useBasketContext} from "../../context/BasketContext.tsx";
 
-function OrderSummary({ items }: BasketSummaryProps) {
-  return (
-    <div id="summary-wrapper">
-      <div id="summary-container">
-        <h2>Order Summary</h2>
-        <ul className="item-list">
-          {items.map((item, index) => (
-            <SummaryItem key={index} item={item} />
-          ))}
-        </ul>
-        <BasketSummary items={items} />
-      </div>
-    </div>
-  );
+function OrderSummary() {
+    const { items } = useBasketContext();
+
+    return (
+        <div id="summary-wrapper">
+            <div id="summary-container">
+                <h2>Order Summary</h2>
+                <ul className="item-list">
+                    {items.map((item, index) => (
+                        <SummaryItem key={index} item={item} />
+                    ))}
+                </ul>
+                <BasketSummary items={items} />
+            </div>
+        </div>
+    );
 }
 
 export default OrderSummary;
