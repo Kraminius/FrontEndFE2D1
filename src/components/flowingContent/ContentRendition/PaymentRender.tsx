7d698fe1c292415549c943f786ac6517fafc9376
mikkelJurs <s224279@dtu.dk>
2024-04-29 15:35:06 +0200

    Removed unused statement.

--- src/components/flowingContent/ContentRendition/PaymentRender.tsx
+++ src/components/flowingContent/ContentRendition/PaymentRender.tsx
@@ -1,38 +1,42 @@
-import { useState } from "react";
-import { BackButton, ContinueButton } from "../Buttons.tsx";
+import { useEffect } from "react";
 import PaymentPage from "../payment/PaymentPage.tsx";
 import { useNavigate } from "react-router-dom";
 import { usePaymentDispatchContext } from "../../../context/PaymentContext.tsx";
 
 export function PaymentRender() {
-	const navigate = useNavigate();
-	const [isContinueDisabled, setIsContinueDisabled] = useState(false);
-	const paymentDispatch = usePaymentDispatchContext();
+  const navigate = useNavigate();
+  const paymentDispatch = usePaymentDispatchContext();
 
-	function handleNextClick() {
-		paymentDispatch({ type: "SET_PAYMENT_COMPLETED", payload: true });
-		navigate("/receipt");
-		window.scrollTo(0, 0);
-	}
-	function handleBackClick() {
-		paymentDispatch({ type: "SET_IS_MOBILE_PAY_VALID", payload: false });
-		paymentDispatch({ type: "SET_IS_CARD_VALID", payload: false });
-		paymentDispatch({ type: "SET_IS_GIFT_CARD_VALID", payload: false });
-		setIsContinueDisabled(true);
-		navigate("/delivery");
-		window.scrollTo(0, 0);
-	}
+  function handleNextClick() {
+    paymentDispatch({ type: "SET_PAYMENT_COMPLETED", payload: true });
+    navigate("/receipt");
+    window.scrollTo(0, 0);
+  }
+  function handleBackClick() {
+    paymentDispatch({ type: "SET_IS_MOBILE_PAY_VALID", payload: false });
+    paymentDispatch({ type: "SET_IS_CARD_VALID", payload: false });
+    paymentDispatch({ type: "SET_IS_GIFT_CARD_VALID", payload: false });
+    paymentDispatch({ type: "SET_IS_VALID", payload: false });
+    navigate("/delivery");
+    window.scrollTo(0, 0);
+  }
+  const resetValidationStates = () => {
+    paymentDispatch({ type: "SET_IS_CARD_VALID", payload: false });
+    paymentDispatch({ type: "SET_IS_MOBILE_PAY_VALID", payload: false });
+    paymentDispatch({ type: "SET_IS_GIFT_CARD_VALID", payload: false });
+    paymentDispatch({ type: "SET_IS_VALID", payload: false });
+  };
 
-	return (
-		<div>
-			<PaymentPage setIsContinueDisabled={setIsContinueDisabled} />
-			<ContinueButton
-				onClick={handleNextClick}
-				isDisabled={isContinueDisabled}
-			/>
-			<BackButton onClick={handleBackClick} />
-		</div>
-	);
+  useEffect(() => {
+    resetValidationStates();
+  });
+
+  return (
+    <PaymentPage
+      handleNextClick={handleNextClick}
+      handleBackClick={handleBackClick}
+    />
+  );
 }
 
 export default PaymentRender;
