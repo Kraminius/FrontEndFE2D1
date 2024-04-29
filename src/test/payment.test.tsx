3cb17cc151caf721e15ec471fba8299532c17f3d
JakobLarsNaurHansen <s224312@student.dtu.dk>
2024-04-29 10:27:29 +0200

    initial rests

--- src/test/payment.test.tsx
+++ src/test/payment.test.tsx
@@ -1,25 +1,50 @@
-import { describe, it, expect, vi, beforeEach } from "vitest";
-import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
+import { afterEach, beforeEach, describe, expect, test, vi } from "vitest";
 import PaymentPage from "../components/flowingContent/payment/PaymentPage";
+import { PaymentProvider, usePaymentContext } from "../context/PaymentContext";
 
+const mockDispatch = vi.fn();
 const mockIsContinueDisabled = vi.fn();
+const paymentContextMock = {
+  isCardValid: false,
+  isMobilePayValid: false,
+  isGiftCardValid: false,
+  activeOption: "",
+  dispatch: mockDispatch,
+};
+function PaymentPageTest() {
+  return (
+    <PaymentProvider>
+      <PaymentPage isContinueDisabled={mockIsContinueDisabled} />
+    </PaymentProvider>
+  );
+}
 
 describe("PaymentPage component", () => {
+  afterEach(cleanup);
+
   beforeEach(() => {
-    render(<PaymentPage isContinueDisabled={mockIsContinueDisabled} />);
+    render(<PaymentPageTest />);
   });
 
-  it("should toggle payment options correctly", () => {
-    const cardOption = screen.getByText(/^Card$/);
-    const mobilePayOption = screen.getByText(/MobilePay/);
-    fireEvent.click(cardOption);
-    fireEvent.click(mobilePayOption);
-
-    expect(screen.getByText(/BuyStuff GiftCard/i)).toBeInTheDocument();
+  test("renders the PaymentPage component with all payment options", () => {
+    expect(screen.getByText(/Choose a Payment Option/i)).toBeInTheDocument();
+    expect(screen.getByText(/^Card$/)).toBeInTheDocument();
+    expect(screen.getByText(/MobilePay/i)).toBeInTheDocument();
+    expect(screen.getByText(/Add A Gift Card/i)).toBeInTheDocument();
+    expect(screen.getByText(/BuyStuff Giftcard/i)).toBeInTheDocument();
   });
 
-  it("should enable continue button when a valid payment option is selected", () => {
-    fireEvent.click(screen.getByText(/mobilepay/i)); // Assuming this triggers validation
-    expect(mockIsContinueDisabled).toHaveBeenCalledWith(true);
+  test("Update internal state when payment option changes", () => {
+    const cardsButton = screen.getByText(/^Card$/);
+    fireEvent.click(cardsButton); // Simulate selecting "Card"
+    paymentContextMock.activeOption = "Cards"; // State update
+    expect(paymentContextMock.activeOption).toBe("Cards");
+
+    const mobilePayButton = screen.getByText(/MobilePay/);
+    fireEvent.click(mobilePayButton); // Simulate selecting "MobilePay"
+    paymentContextMock.activeOption = "MobilePay"; // State update
+    expect(paymentContextMock.activeOption).toBe("MobilePay");
   });
 });
