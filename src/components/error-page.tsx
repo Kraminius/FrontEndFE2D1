99f869d4c6421e09fdd8782476349c1a596753d2
mikkelJurs <s224279@dtu.dk>
2024-04-29 18:52:55 +0200

    defined type for building.

--- src/components/error-page.tsx
+++ src/components/error-page.tsx
@@ -1,7 +1,7 @@
 import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
-  const error = useRouteError();
+  const error = useRouteError() as Error & { statusText?: string };
   console.error(error);
 
   return (
