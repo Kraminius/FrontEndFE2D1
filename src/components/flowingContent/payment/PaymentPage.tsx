a4e4175ca5b44e4fea19157954ee1b6050e973af
JakobLarsNaurHansen <s224312@student.dtu.dk>
2024-04-29 08:50:09 +0200

    Fixed the loop in card update

--- src/components/flowingContent/payment/PaymentPage.tsx
+++ src/components/flowingContent/payment/PaymentPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import CardInputs from "./CardInformation.tsx";
 import MobilePayInputs from "./MobilePayInformation.tsx";
 import GiftCardInputs from "./GiftCardInformation.tsx";
@@ -10,6 +10,7 @@ import {
   usePaymentContext,
   usePaymentDispatchContext,
 } from "../../../context/PaymentContext.tsx";
+import { Action } from "../../../context/BasketContext.tsx";
 
 interface PaymentPageProps {
   isContinueDisabled: (isValid: boolean) => void;
@@ -22,17 +23,26 @@ function PaymentPage({ isContinueDisabled }: PaymentPageProps) {
   const { activeOption, isGiftCardValid, isMobilePayValid, isCardValid } =
     paymentState;
 
-  const handleOptionClick = (option: string) => {
-    paymentDispatch({ type: "SET_ACTIVE_OPTION", payload: option });
-  };
+  const handleOptionClick = useCallback(
+    (option: string) => {
+      paymentDispatch({ type: "SET_ACTIVE_OPTION", payload: option });
+    },
+    [paymentDispatch]
+  );
 
-  const setIsCardValid = (isValid: boolean) => {
-    paymentDispatch({ type: "SET_IS_CARD_VALID", payload: isValid });
-  };
+  const setIsCardValid = useCallback(
+    (isValid: boolean) => {
+      paymentDispatch({ type: "SET_IS_CARD_VALID", payload: isValid });
+    },
+    [paymentDispatch]
+  );
 
-  const setIsMobilePayValid = (isValid: boolean) => {
-    paymentDispatch({ type: "SET_IS_MOBILE_PAY_VALID", payload: isValid });
-  };
+  const setIsMobilePayValid = useCallback(
+    (isValid: boolean) => {
+      paymentDispatch({ type: "SET_IS_MOBILE_PAY_VALID", payload: isValid });
+    },
+    [paymentDispatch]
+  );
 
   useEffect(() => {
     let valid = false;
@@ -46,8 +56,10 @@ function PaymentPage({ isContinueDisabled }: PaymentPageProps) {
       default:
         valid = isGiftCardValid;
     }
-    paymentDispatch({ type: "SET_IS_VALID", payload: valid });
-    isContinueDisabled(!valid);
+    if (valid !== paymentState.isValid) {
+      paymentDispatch({ type: "SET_IS_VALID", payload: valid });
+      isContinueDisabled(!valid);
+    }
   }, [
     activeOption,
     isGiftCardValid,
